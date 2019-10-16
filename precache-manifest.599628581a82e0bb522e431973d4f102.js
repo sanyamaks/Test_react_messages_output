efd418a2a614dd6d71c1d37130ae8ddd5c3063b5
Aleksandr <sanyamaks14@gmail.com>
2019-10-17 01:41:49 +0400

    Updates

---
+++ precache-manifest.599628581a82e0bb522e431973d4f102.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8346257cab907bb623c5bfee2c59f61",
+    "url": "/test_react_messages_output/index.html"
+  },
+  {
+    "revision": "012cca16894ffdc1dec8",
+    "url": "/test_react_messages_output/static/css/main.c06b5d59.chunk.css"
+  },
+  {
+    "revision": "2351cadd16bd8175aa9f",
+    "url": "/test_react_messages_output/static/js/2.2c2ac4d9.chunk.js"
+  },
+  {
+    "revision": "012cca16894ffdc1dec8",
+    "url": "/test_react_messages_output/static/js/main.75c7df52.chunk.js"
+  },
+  {
+    "revision": "5d41a8362ad384b65faa",
+    "url": "/test_react_messages_output/static/js/runtime-main.330acd53.js"
+  },
+  {
+    "revision": "9e48e6c5e59f6a3a075cd89f3a9cf855",
+    "url": "/test_react_messages_output/static/media/Muli-Bold.9e48e6c5.ttf"
+  },
+  {
+    "revision": "5979272a12758a993d09431446d5295f",
+    "url": "/test_react_messages_output/static/media/Muli-ExtraBold.5979272a.ttf"
+  },
+  {
+    "revision": "e957eea5f4b556bb57724b120e94047f",
+    "url": "/test_react_messages_output/static/media/Muli-Regular.e957eea5.ttf"
+  },
+  {
+    "revision": "f5312cf98dd008c25289c0e939e40ea2",
+    "url": "/test_react_messages_output/static/media/Muli-SemiBold.f5312cf9.ttf"
+  },
+  {
+    "revision": "71e487dfb2a608fabbb907867c5ca333",
+    "url": "/test_react_messages_output/static/media/header-background.71e487df.svg"
+  },
+  {
+    "revision": "4fe8a6a6f193691ac2da265b4e3619f3",
+    "url": "/test_react_messages_output/static/media/search-icon.4fe8a6a6.svg"
+  }
+]);
